3ba66edfa5ae069fbd937c4cb17937f1168b3687
Jorden_Reuter <149687553+JordenReuter@users.noreply.github.com>
2025-04-28 08:30:05 +0200

    fix: readded all tests

--- src/test/java/org/tkit/onecx/iam/rs/external/v1/controllers/AdminRestControllerV1Test.java
+++ src/test/java/org/tkit/onecx/iam/rs/external/v1/controllers/AdminRestControllerV1Test.java
@@ -1,82 +1,82 @@
-//package org.tkit.onecx.iam.rs.external.v1.controllers;
-//
-//import static io.restassured.RestAssured.given;
-//import static jakarta.ws.rs.core.MediaType.APPLICATION_JSON;
-//
-//import java.io.IOException;
-//import java.util.Base64;
-//
-//import jakarta.ws.rs.core.Response;
-//
-//import org.jose4j.json.internal.json_simple.JSONObject;
-//import org.junit.jupiter.api.Assertions;
-//import org.junit.jupiter.api.Test;
-//import org.tkit.onecx.iam.test.AbstractTest;
-//import org.tkit.quarkus.security.test.GenerateKeycloakClient;
-//
-//import com.fasterxml.jackson.databind.ObjectMapper;
-//
-//import gen.org.tkit.onecx.iam.v1.model.UserRolesResponseDTOV1;
-//import gen.org.tkit.onecx.iam.v1.model.UserRolesSearchRequestDTOV1;
-//import io.quarkus.test.common.http.TestHTTPEndpoint;
-//import io.quarkus.test.junit.QuarkusTest;
-//import io.quarkus.test.keycloak.client.KeycloakTestClient;
-//
-//@QuarkusTest
-//@TestHTTPEndpoint(AdminRestControllerV1.class)
-//@GenerateKeycloakClient(clientName = "testClient", scopes = { "ocx-ia:read", "ocx-ia:write" })
-//class AdminRestControllerV1Test extends AbstractTest {
-//
-//    private static final KeycloakTestClient keycloakAuthClient = new KeycloakTestClient();
-//    KeycloakTestClient keycloakClient = createClient();
-//    KeycloakTestClient keycloakClient1 = createClient1();
-//
-//    @Test
-//    void getUserRolesTest() throws IOException {
-//        var tokens = this.getTokens(keycloakClient, USER_ALICE);
-//        var aliceToken = tokens.getIdToken();
-//        ObjectMapper mapper = new ObjectMapper();
-//        Base64.Decoder decoder = Base64.getUrlDecoder();
-//        String[] chunks = aliceToken.split("\\.");
-//        String body = new String(decoder.decode(chunks[1]));
-//        JSONObject jwt = mapper.readValue(body, JSONObject.class);
-//
-//        String id = jwt.get("sub").toString();
-//
-//        var tokens1 = this.getTokens(keycloakClient1, USER_ALICE);
-//        var aliceToken1 = tokens1.getIdToken();
-//        String[] chunks1 = aliceToken1.split("\\.");
-//        String body1 = new String(decoder.decode(chunks1[1]));
-//        JSONObject jwt1 = mapper.readValue(body1, JSONObject.class);
-//
-//        var result = given()
-//                .auth().oauth2(keycloakAuthClient.getClientAccessToken("testClient"))
-//                .header(APM_HEADER_TOKEN, aliceToken)
-//                .pathParam("userId", id)
-//                .body(new UserRolesSearchRequestDTOV1().issuer(jwt.get("iss").toString()))
-//                .contentType(APPLICATION_JSON)
-//                .post()
-//                .then().statusCode(Response.Status.OK.getStatusCode())
-//                .extract().as(UserRolesResponseDTOV1.class);
-//        Assertions.assertNotNull(result);
-//        Assertions.assertEquals(2, result.getRoles().size());
-//
-//        //user not found:
-//        given()
-//                .auth().oauth2(keycloakAuthClient.getClientAccessToken("testClient"))
-//                .header(APM_HEADER_TOKEN, aliceToken)
-//                .body(new UserRolesSearchRequestDTOV1().issuer(jwt1.get("iss").toString().replace("quarkus", "master")))
-//                .pathParam("userId", id)
-//                .contentType(APPLICATION_JSON).post()
-//                .then().statusCode(Response.Status.BAD_REQUEST.getStatusCode());
-//
-//        //no token test
-//        given()
-//                .auth().oauth2(keycloakAuthClient.getClientAccessToken("testClient"))
-//                .body(new UserRolesSearchRequestDTOV1().issuer(""))
-//                .pathParam("userId", id)
-//                .contentType(APPLICATION_JSON).post()
-//                .then().statusCode(Response.Status.BAD_REQUEST.getStatusCode());
-//    }
-//
-//}
+package org.tkit.onecx.iam.rs.external.v1.controllers;
+
+import static io.restassured.RestAssured.given;
+import static jakarta.ws.rs.core.MediaType.APPLICATION_JSON;
+
+import java.io.IOException;
+import java.util.Base64;
+
+import jakarta.ws.rs.core.Response;
+
+import org.jose4j.json.internal.json_simple.JSONObject;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.tkit.onecx.iam.test.AbstractTest;
+import org.tkit.quarkus.security.test.GenerateKeycloakClient;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import gen.org.tkit.onecx.iam.v1.model.UserRolesResponseDTOV1;
+import gen.org.tkit.onecx.iam.v1.model.UserRolesSearchRequestDTOV1;
+import io.quarkus.test.common.http.TestHTTPEndpoint;
+import io.quarkus.test.junit.QuarkusTest;
+import io.quarkus.test.keycloak.client.KeycloakTestClient;
+
+@QuarkusTest
+@TestHTTPEndpoint(AdminRestControllerV1.class)
+@GenerateKeycloakClient(clientName = "testClient", scopes = { "ocx-ia:read", "ocx-ia:write" })
+class AdminRestControllerV1Test extends AbstractTest {
+
+    private static final KeycloakTestClient keycloakAuthClient = new KeycloakTestClient();
+    KeycloakTestClient keycloakClient = createClient();
+    KeycloakTestClient keycloakClient1 = createClient1();
+
+    @Test
+    void getUserRolesTest() throws IOException {
+        var tokens = this.getTokens(keycloakClient, USER_ALICE);
+        var aliceToken = tokens.getIdToken();
+        ObjectMapper mapper = new ObjectMapper();
+        Base64.Decoder decoder = Base64.getUrlDecoder();
+        String[] chunks = aliceToken.split("\\.");
+        String body = new String(decoder.decode(chunks[1]));
+        JSONObject jwt = mapper.readValue(body, JSONObject.class);
+
+        String id = jwt.get("sub").toString();
+
+        var tokens1 = this.getTokens(keycloakClient1, USER_ALICE);
+        var aliceToken1 = tokens1.getIdToken();
+        String[] chunks1 = aliceToken1.split("\\.");
+        String body1 = new String(decoder.decode(chunks1[1]));
+        JSONObject jwt1 = mapper.readValue(body1, JSONObject.class);
+
+        var result = given()
+                .auth().oauth2(keycloakAuthClient.getClientAccessToken("testClient"))
+                .header(APM_HEADER_TOKEN, aliceToken)
+                .pathParam("userId", id)
+                .body(new UserRolesSearchRequestDTOV1().issuer(jwt.get("iss").toString()))
+                .contentType(APPLICATION_JSON)
+                .post()
+                .then().statusCode(Response.Status.OK.getStatusCode())
+                .extract().as(UserRolesResponseDTOV1.class);
+        Assertions.assertNotNull(result);
+        Assertions.assertEquals(2, result.getRoles().size());
+
+        //user not found:
+        given()
+                .auth().oauth2(keycloakAuthClient.getClientAccessToken("testClient"))
+                .header(APM_HEADER_TOKEN, aliceToken)
+                .body(new UserRolesSearchRequestDTOV1().issuer(jwt1.get("iss").toString().replace("quarkus", "master")))
+                .pathParam("userId", id)
+                .contentType(APPLICATION_JSON).post()
+                .then().statusCode(Response.Status.BAD_REQUEST.getStatusCode());
+
+        //no token test
+        given()
+                .auth().oauth2(keycloakAuthClient.getClientAccessToken("testClient"))
+                .body(new UserRolesSearchRequestDTOV1().issuer(""))
+                .pathParam("userId", id)
+                .contentType(APPLICATION_JSON).post()
+                .then().statusCode(Response.Status.BAD_REQUEST.getStatusCode());
+    }
+
+}
--- src/test/java/org/tkit/onecx/iam/rs/internal/controllers/UserRestControllerTest.java
+++ src/test/java/org/tkit/onecx/iam/rs/internal/controllers/UserRestControllerTest.java
@@ -1,160 +1,160 @@
-//package org.tkit.onecx.iam.rs.internal.controllers;
-//
-//import static io.restassured.RestAssured.given;
-//import static jakarta.ws.rs.core.MediaType.APPLICATION_JSON;
-//import static org.assertj.core.api.Assertions.assertThat;
-//import static org.tkit.onecx.iam.rs.internal.mappers.ExceptionMapper.ErrorKeys.TOKEN_ERROR;
-//
-//import jakarta.ws.rs.core.Response;
-//
-//import org.junit.jupiter.api.Assertions;
-//import org.junit.jupiter.api.Test;
-//import org.tkit.onecx.iam.rs.internal.mappers.ExceptionMapper;
-//import org.tkit.onecx.iam.test.AbstractTest;
-//import org.tkit.quarkus.security.test.GenerateKeycloakClient;
-//
-//import gen.org.tkit.onecx.iam.internal.model.ProblemDetailResponseDTO;
-//import gen.org.tkit.onecx.iam.internal.model.ProvidersResponseDTO;
-//import gen.org.tkit.onecx.iam.internal.model.UserResetPasswordRequestDTO;
-//import io.quarkus.test.common.http.TestHTTPEndpoint;
-//import io.quarkus.test.junit.QuarkusTest;
-//import io.quarkus.test.keycloak.client.KeycloakTestClient;
-//
-//@QuarkusTest
-//@TestHTTPEndpoint(UserRestController.class)
-//@GenerateKeycloakClient(clientName = "testClient", scopes = { "ocx-ia:read", "ocx-ia:write", "ocx-ia:all" })
-//public class UserRestControllerTest extends AbstractTest {
-//    KeycloakTestClient authClient = new KeycloakTestClient();
-//    KeycloakTestClient keycloakClient = createClient();
-//
-//    @Test
-//    void resetPasswordTest() {
-//        var tokens = this.getTokens(keycloakClient, USER_ALICE);
-//        var aliceToken = tokens.getIdToken();
-//        UserResetPasswordRequestDTO dto = new UserResetPasswordRequestDTO();
-//        dto.setPassword("changedPassword");
-//
-//        given()
-//                .auth().oauth2(authClient.getClientAccessToken("testClient"))
-//                .contentType(APPLICATION_JSON)
-//                .header(APM_HEADER_TOKEN, aliceToken)
-//                .body(dto)
-//                .put("password")
-//                .then()
-//                .statusCode(Response.Status.NO_CONTENT.getStatusCode());
-//
-//        aliceToken = getTokens(keycloakClient, USER_ALICE, dto.getPassword()).getIdToken();
-//        dto.setPassword(USER_ALICE);
-//
-//        given()
-//                .auth().oauth2(authClient.getClientAccessToken("testClient"))
-//                .contentType(APPLICATION_JSON)
-//                .header(APM_HEADER_TOKEN, aliceToken)
-//                .body(dto)
-//                .put("password")
-//                .then()
-//                .statusCode(Response.Status.NO_CONTENT.getStatusCode());
-//
-//    }
-//
-//    @Test
-//    void getUsersProviderAndRealm() {
-//        var tokens = this.getTokens(keycloakClient, USER_ALICE);
-//        var aliceToken = tokens.getIdToken();
-//
-//        var res = given()
-//                .auth().oauth2(authClient.getClientAccessToken("testClient"))
-//                .contentType(APPLICATION_JSON)
-//                .header(APM_HEADER_TOKEN, aliceToken)
-//                .get("provider")
-//                .then()
-//                .statusCode(Response.Status.OK.getStatusCode())
-//                .extract().as(ProvidersResponseDTO.class);
-//        assertThat(res.getProviders()).hasSize(1);
-//        assertThat(res.getProviders().get(0).getName()).isEqualTo("kc0");
-//        assertThat(res.getProviders().get(0).getDomains()).hasSize(1);
-//        assertThat(res.getProviders().get(0).getDomains().get(0).getName()).isEqualTo("quarkus");
-//    }
-//
-//    @Test
-//    void getUsersProviderAndRealm_empty_token_Test() {
-//        given()
-//                .auth().oauth2(authClient.getClientAccessToken("testClient"))
-//                .contentType(APPLICATION_JSON)
-//                .header(APM_HEADER_TOKEN, " ")
-//                .get("provider")
-//                .then()
-//                .statusCode(Response.Status.BAD_REQUEST.getStatusCode())
-//                .extract().as(ProblemDetailResponseDTO.class);
-//    }
-//
-//    @Test
-//    void resetPasswordNoTokenTest() {
-//
-//        UserResetPasswordRequestDTO dto = new UserResetPasswordRequestDTO();
-//        dto.setPassword("*******");
-//
-//        var exception = given()
-//                .auth().oauth2(authClient.getClientAccessToken("testClient"))
-//                .contentType(APPLICATION_JSON)
-//                .body(dto)
-//                .put("password")
-//                .then()
-//                .statusCode(Response.Status.BAD_REQUEST.getStatusCode())
-//                .extract()
-//                .body().as(ProblemDetailResponseDTO.class);
-//
-//        Assertions.assertNotNull(exception);
-//        Assertions.assertEquals(TOKEN_ERROR.name(), exception.getErrorCode());
-//        Assertions.assertEquals(
-//                "Principal token is required",
-//                exception.getDetail());
-//        assertThat(exception.getInvalidParams()).isNotNull().isEmpty();
-//    }
-//
-//    @Test
-//    void resetPasswordEmptyRequestTest() {
-//        UserResetPasswordRequestDTO dto = new UserResetPasswordRequestDTO();
-//
-//        var exception = given()
-//                .auth().oauth2(authClient.getClientAccessToken("testClient"))
-//                .contentType(APPLICATION_JSON)
-//                .header(APM_HEADER_TOKEN, this.getTokens(keycloakClient, USER_ALICE).getIdToken())
-//                .body(dto)
-//                .put("password")
-//                .then()
-//                .statusCode(Response.Status.BAD_REQUEST.getStatusCode())
-//                .extract()
-//                .body().as(ProblemDetailResponseDTO.class);
-//
-//        Assertions.assertNotNull(exception);
-//        Assertions.assertEquals(ExceptionMapper.ErrorKeys.CONSTRAINT_VIOLATIONS.name(),
-//                exception.getErrorCode());
-//        Assertions.assertEquals(
-//                "resetPassword.userResetPasswordRequestDTO.password: must not be null",
-//                exception.getDetail());
-//        Assertions.assertNotNull(exception.getInvalidParams());
-//    }
-//
-//    @Test
-//    void resetPasswordNoRequestTest() {
-//
-//        var exception = given()
-//                .auth().oauth2(authClient.getClientAccessToken("testClient"))
-//                .contentType(APPLICATION_JSON)
-//                .put("password")
-//                .then()
-//                .statusCode(Response.Status.BAD_REQUEST.getStatusCode())
-//                .extract()
-//                .body().as(ProblemDetailResponseDTO.class);
-//
-//        Assertions.assertNotNull(exception);
-//        Assertions.assertEquals(
-//                ExceptionMapper.ErrorKeys.CONSTRAINT_VIOLATIONS.name(),
-//                exception.getErrorCode());
-//        Assertions.assertEquals(
-//                "resetPassword.userResetPasswordRequestDTO: must not be null",
-//                exception.getDetail());
-//        Assertions.assertNotNull(exception.getInvalidParams());
-//    }
-//}
+package org.tkit.onecx.iam.rs.internal.controllers;
+
+import static io.restassured.RestAssured.given;
+import static jakarta.ws.rs.core.MediaType.APPLICATION_JSON;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.tkit.onecx.iam.rs.internal.mappers.ExceptionMapper.ErrorKeys.TOKEN_ERROR;
+
+import jakarta.ws.rs.core.Response;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.tkit.onecx.iam.rs.internal.mappers.ExceptionMapper;
+import org.tkit.onecx.iam.test.AbstractTest;
+import org.tkit.quarkus.security.test.GenerateKeycloakClient;
+
+import gen.org.tkit.onecx.iam.internal.model.ProblemDetailResponseDTO;
+import gen.org.tkit.onecx.iam.internal.model.ProvidersResponseDTO;
+import gen.org.tkit.onecx.iam.internal.model.UserResetPasswordRequestDTO;
+import io.quarkus.test.common.http.TestHTTPEndpoint;
+import io.quarkus.test.junit.QuarkusTest;
+import io.quarkus.test.keycloak.client.KeycloakTestClient;
+
+@QuarkusTest
+@TestHTTPEndpoint(UserRestController.class)
+@GenerateKeycloakClient(clientName = "testClient", scopes = { "ocx-ia:read", "ocx-ia:write", "ocx-ia:all" })
+public class UserRestControllerTest extends AbstractTest {
+    KeycloakTestClient authClient = new KeycloakTestClient();
+    KeycloakTestClient keycloakClient = createClient();
+
+    @Test
+    void resetPasswordTest() {
+        var tokens = this.getTokens(keycloakClient, USER_ALICE);
+        var aliceToken = tokens.getIdToken();
+        UserResetPasswordRequestDTO dto = new UserResetPasswordRequestDTO();
+        dto.setPassword("changedPassword");
+
+        given()
+                .auth().oauth2(authClient.getClientAccessToken("testClient"))
+                .contentType(APPLICATION_JSON)
+                .header(APM_HEADER_TOKEN, aliceToken)
+                .body(dto)
+                .put("password")
+                .then()
+                .statusCode(Response.Status.NO_CONTENT.getStatusCode());
+
+        aliceToken = getTokens(keycloakClient, USER_ALICE, dto.getPassword()).getIdToken();
+        dto.setPassword(USER_ALICE);
+
+        given()
+                .auth().oauth2(authClient.getClientAccessToken("testClient"))
+                .contentType(APPLICATION_JSON)
+                .header(APM_HEADER_TOKEN, aliceToken)
+                .body(dto)
+                .put("password")
+                .then()
+                .statusCode(Response.Status.NO_CONTENT.getStatusCode());
+
+    }
+
+    @Test
+    void getUsersProviderAndRealm() {
+        var tokens = this.getTokens(keycloakClient, USER_ALICE);
+        var aliceToken = tokens.getIdToken();
+
+        var res = given()
+                .auth().oauth2(authClient.getClientAccessToken("testClient"))
+                .contentType(APPLICATION_JSON)
+                .header(APM_HEADER_TOKEN, aliceToken)
+                .get("provider")
+                .then()
+                .statusCode(Response.Status.OK.getStatusCode())
+                .extract().as(ProvidersResponseDTO.class);
+        assertThat(res.getProviders()).hasSize(1);
+        assertThat(res.getProviders().get(0).getName()).isEqualTo("kc0");
+        assertThat(res.getProviders().get(0).getDomains()).hasSize(1);
+        assertThat(res.getProviders().get(0).getDomains().get(0).getName()).isEqualTo("quarkus");
+    }
+
+    @Test
+    void getUsersProviderAndRealm_empty_token_Test() {
+        given()
+                .auth().oauth2(authClient.getClientAccessToken("testClient"))
+                .contentType(APPLICATION_JSON)
+                .header(APM_HEADER_TOKEN, " ")
+                .get("provider")
+                .then()
+                .statusCode(Response.Status.BAD_REQUEST.getStatusCode())
+                .extract().as(ProblemDetailResponseDTO.class);
+    }
+
+    @Test
+    void resetPasswordNoTokenTest() {
+
+        UserResetPasswordRequestDTO dto = new UserResetPasswordRequestDTO();
+        dto.setPassword("*******");
+
+        var exception = given()
+                .auth().oauth2(authClient.getClientAccessToken("testClient"))
+                .contentType(APPLICATION_JSON)
+                .body(dto)
+                .put("password")
+                .then()
+                .statusCode(Response.Status.BAD_REQUEST.getStatusCode())
+                .extract()
+                .body().as(ProblemDetailResponseDTO.class);
+
+        Assertions.assertNotNull(exception);
+        Assertions.assertEquals(TOKEN_ERROR.name(), exception.getErrorCode());
+        Assertions.assertEquals(
+                "Principal token is required",
+                exception.getDetail());
+        assertThat(exception.getInvalidParams()).isNotNull().isEmpty();
+    }
+
+    @Test
+    void resetPasswordEmptyRequestTest() {
+        UserResetPasswordRequestDTO dto = new UserResetPasswordRequestDTO();
+
+        var exception = given()
+                .auth().oauth2(authClient.getClientAccessToken("testClient"))
+                .contentType(APPLICATION_JSON)
+                .header(APM_HEADER_TOKEN, this.getTokens(keycloakClient, USER_ALICE).getIdToken())
+                .body(dto)
+                .put("password")
+                .then()
+                .statusCode(Response.Status.BAD_REQUEST.getStatusCode())
+                .extract()
+                .body().as(ProblemDetailResponseDTO.class);
+
+        Assertions.assertNotNull(exception);
+        Assertions.assertEquals(ExceptionMapper.ErrorKeys.CONSTRAINT_VIOLATIONS.name(),
+                exception.getErrorCode());
+        Assertions.assertEquals(
+                "resetPassword.userResetPasswordRequestDTO.password: must not be null",
+                exception.getDetail());
+        Assertions.assertNotNull(exception.getInvalidParams());
+    }
+
+    @Test
+    void resetPasswordNoRequestTest() {
+
+        var exception = given()
+                .auth().oauth2(authClient.getClientAccessToken("testClient"))
+                .contentType(APPLICATION_JSON)
+                .put("password")
+                .then()
+                .statusCode(Response.Status.BAD_REQUEST.getStatusCode())
+                .extract()
+                .body().as(ProblemDetailResponseDTO.class);
+
+        Assertions.assertNotNull(exception);
+        Assertions.assertEquals(
+                ExceptionMapper.ErrorKeys.CONSTRAINT_VIOLATIONS.name(),
+                exception.getErrorCode());
+        Assertions.assertEquals(
+                "resetPassword.userResetPasswordRequestDTO: must not be null",
+                exception.getDetail());
+        Assertions.assertNotNull(exception.getInvalidParams());
+    }
+}
